2130d5a659076c9adf4d401ef76b19a5a0d379d1
Nestor Napoles <napulen@gmail.com>
2017-06-18 04:35:04 -0500

    Adding license

--- harmparser.py
+++ harmparser.py
@@ -1,7 +1,20 @@
 '''
 A parser for the **harm syntax based on regular expressions
 
-Nestor Napoles, June 2017
+Copyright 2017 Nestor Napoles
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
 '''
 
 import re
@@ -85,6 +98,7 @@ def parseHarmExpr(harmexpr):
 		harmdict['implied'] = True
 	return harmdict
 
+
 def parseHarm(harm):
 	# Separate possible secondary functions
 	exprs = harm.split('/')
@@ -97,8 +111,6 @@ def parseHarm(harm):
 			functions.append(harmdict)
 	pp.pprint(functions)
 
-
-	
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parses an expression in **harm syntax and describes its content')
